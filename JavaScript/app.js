51f06a569a7416bfd77235b06ff2d8722718b170
Hamza Belovodjanin <hbelovodjanin@gmail.com>
2022-05-09 17:34:07 +0200

    JS

---
+++ JavaScript/app.js
@@ -0,0 +1,20 @@
+alert("radi")
+
+console.log("Prvi ispis u JS konzoli")
+'nesto' //string
+65 //number
+true,false //boolean
+["string", 76, true, ["string", 19]] //boolean
+null //ne postoji informacija
+undefined // podatak postoji ali nije definisan
+{} 
+
+var ime = "ime je:Isko"
+console.log(ime)
+ime = 128
+console.log (ime)
+// varijable su skaldiste informacija
+
+console.log (2+2)
+
+console.log ("javascript" + "je cudna")
