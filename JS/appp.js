b7171866cb9a04fd065b5711394ae7a0eefcdbbc
Hamza Belovodjanin <hbelovodjanin@gmail.com>
2022-05-11 18:46:51 +0200

    JS

---
+++ JS/appp.js
@@ -0,0 +1,105 @@
+//LET I CONST;
+// Let moze da se menja
+// Const ne moze da se menja konstantan je
+
+// const ime = "Max"
+// Ime = 'Isko' - GRESKA
+
+
+// let godine = 30;
+// console.log(ime + " " + godine)
+
+// godine = 25
+// console.log(ime + " " + godine)
+
+// console.log(5 === 5) KOD PROVERE 3 put jednako
+// console.log(5 !== 6) KOD NEGACIJE 2 put jednako
+// console.log(6 <= 50) Vece,manje ili jednako7
+
+
+// const ime = prompt("Unesi tvoje ime")
+// const godine = prompt("Unesi tvoje godine")
+// alert ("Ziveo si " + godine * 12 + "meseci")
+
+// const ime = 'Max'
+// const godine = 40;
+// const meseci = godine * 12
+// console.log (meseci)
+
+
+// prefixni i postfixni operatori -
+// Prefixni odradi odma operaciju
+// Postfixni je pokupi i odradi je u sledecu operaciju
+
+// let age = 20
+// console.log(age++)
+// console.log(age)
+
+// age = age - 1
+
+
+// const age = 17
+// if (age >= 18) {
+//     console.log("Punoletan si")
+// }
+
+// else if (age < 18) {
+//     console.log("Nisi punoletan")
+// }
+
+// else {
+
+//     console.log ("Izvrsava se samo u slucaju kada ni jedan uslov nije zadovoljen")
+// }
+
+
+// const age = prompt("Unesi tvoje godine")
+
+// if(+age === 18) {
+
+//     console.log ("Tek si postao punoletan")
+// }
+// else if (age > 18) {
+
+//     console.log("Ti si odrastao")
+// }
+
+// else if (age > 12) {
+
+//     console.log("Ti si tinejdzer")
+// }
+
+// else {
+//     console.log("Ti si dete")
+// }
+
+
+// const a = 4
+// const b = 5
+// const c = 0
+// console.log (a && b && c) // Mora svaki da je true da bi bilo true ili vraca onaj zadnji (AND OPERATOR)
+
+// const a = 5
+// const b = 10
+
+// console.log ( a || b) // Or ( || ) operator radi samo ako je jedno tacno (or)
+// console.log(!5 ||  !false ) // ( ! ) se koristi za negaciju, ako je nesto true bice false ako bude flase bice true
+
+
+// const age = prompt("unesi tvoje godine")
+// if (+age >= 18 & age < 40 ) { 
+//     console.log ("Odrastao si")
+// }
+// else if (+age > 11 || +age < 18) {
+
+//     console.log("tinejdzer") 
+// }
+
+// else if (+age > 40)
+//  {console.log ("covek")
+// }
+
+// else { console.log ("dete")
+// }
+
+
---
+++ JS/petlje/app.js
@@ -0,0 +1,28 @@
+// for petlja
+// while
+// do while
+// i-- smanji za jedan
+// i++ povecaj za jedan
+
+
+// let suma = 0
+// for (let i=0; i < 20; i++) {
+//     console.log ("Brojac", i)
+//     if (i % 2 === 0 ) // RACUNANJE PARNOG BROJA
+//     console.log ("Brojac unutar uslova", i )
+//     suma += i;
+// }
+// console.log ("SUMA", suma)
+
+// FIZZBUZZ CHALLANGE
+// 0-100; 
+// 3 - FIZZ
+// 5 - BUZZ
+// 3 i 5 - FIZZBUZz
+let n = 20
+
+while (n === 20) {
+    console.log ("Unutar while petlje")
+    break;
+}
+n = 21 
