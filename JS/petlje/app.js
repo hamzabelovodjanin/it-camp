d9e930a1b69c45f8341ec8f2e140388ce0192c56
Hamza Belovodjanin <hbelovodjanin@gmail.com>
2022-05-16 18:40:04 +0200

    JS

--- JS/petlje/app.js
+++ JS/petlje/app.js
@@ -27,16 +27,61 @@
 // }
 // n = 21 
 
-let suma= 0;
-for (let i = 0; i <100; i++) {
-    if ( i % 3 === 0 && i % 5 === 0)
-    console.log("fizzBuzz",i )
-   else if (i % 3 === 0)
-    console.log("Fizz", i);
-   else if ( i % 5 == 0)
-    console.log("Buzz",i);
-    else {
-        console.log(i);
-    }
 
-}
+// for (let i = 0; i <100; i++) {
+//     if ( i % 3 === 0 && i % 5 === 0)
+//     console.log("fizzBuzz",i )
+//    else if (i % 3 === 0)
+//     console.log("Fizz", i);
+//    else if ( i % 5 == 0)
+//     console.log("Buzz",i);
+//     else {
+//         console.log(i);
+//     }
+
+// }
+
+// const rec = 'Sta smo radili danAs'
+// let counter = 0
+
+// for (let  i = 0; i < rec.length ; i++) {
+//    if (rec[i] === "a" || rec[i] === "A") {
+//        counter++
+
+//    }
+
+    
+
+// }
+// console.log(counter)
+
+
+// const string = 'ovo je string'
+// let newString = ""
+
+// for (let i = 0; i < string.length; i++) {
+
+    // const element = string[i];
+
+    // if(i === string.length -1){
+    //     newString += element.toUpperCase()
+    //  } 
+    //     else if (string[i+1] === " "){
+    //         newString += element.toUpperCase()
+    //         }
+        
+    //     else {
+    //             newString += element
+    //         }
+    // }
+    // const string = 'ovo je string'
+    // let newString = ""
+
+    // for (let i = 0; i < string.length; i++) {
+
+
+
+    // }
+
+    // console.log(newString)
+
---
+++ JS/switch statemant/app.js
@@ -0,0 +1,63 @@
+// let godine = 15
+
+// switch (godine) {
+//     case 20:
+//     console.log ("Ima 20  godina")
+//     break;
+//     case 15:
+//     console.log ("Ima 15  godina")
+//     break;
+//     default:
+//         console.log ("Nije upao ni u jedan case")
+
+// }
+// if(isSunny) // if(isSunny === true)
+// if(!isSunny) // if(isSunny !== false) || // if(isSunny === false)
+
+
+
+// if(isSunny) {
+//     console.log ("Ponesi naocare")
+// }
+// else {
+//     console.log("Nemoj nositi naocare")
+// }
+
+// TERNARY OPERATOR
+// const isSunny = true
+// isSunny ? console.log("Danas je suncano") : console.log("Nije suncano")
+
+
+// let godine = 19
+
+// let daLiJePunoletan = (godine >=18) ? true : false 
+// console.log("Punoletan", daLiJePunoletan)
+
+if("") {
+
+    console.log ("Tacna vrenosti ili truthy")
+
+}
+else {
+    console.log("Netacna vrednost ili falsy")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
