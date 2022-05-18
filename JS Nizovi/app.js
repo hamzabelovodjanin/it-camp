4213e0e50a4d68451d228a39d4a87023e8a27bf0
Hamza Belovodjanin <hbelovodjanin@gmail.com>
2022-05-18 18:35:15 +0200

    nizovi

---
+++ JS Nizovi/app.js
@@ -0,0 +1,28 @@
+// NIZOVI
+// NIZ I INDEX Pocinje od 0 a zavrsava se lenght -1
+// PRVI CLAN U NIZU JE UVEK 0
+// []- NIZ
+// {}- objekat
+
+          
+// cars [0] = 3
+// console.log("Pre menjanja", cars)
+// cars.length = 3
+
+
+// const niz = ["BMW", 4, undefined, "VW", null, NaN, false, true,"sadas","kurs"]
+
+// for (let clan in niz){
+//     console.log("Index", clan , "Vrednost funkcije", niz[clan])
+
+
+
+// }
+
+const studenti = ["Marko", "Janko", "Sasa"]
+studenti [8] = "Zika"
+console.log(studenti)
+
+
+const druginiz = new Array("Clan1", "Clan2", "Clan3")
+console.log(druginiz)
